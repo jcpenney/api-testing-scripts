f89dd298b9e8e767872c93a0bd84bf6f095db611
Nick Cherry <nickcherryjiggz@gmail.com>
2014-08-29 17:44:49 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Installing Dependencies
+
+After installing [Node](http://nodejs.org/) and [NPM](https://github.com/npm/npm), run `npm install` from the project's root to install all dependencies.
+
+
+## Testling Delete Address Service
+
+Run `node delete-first-address` to execute the script. You will be prompted to authenticate with your JCP credentials, then the first existing address will be deleted from the account.
---
+++ delete-first-address.js
@@ -0,0 +1,103 @@
+/****************************************************************/
+/* Imports */
+/****************************************************************/
+var Colors = require('colors');
+var Prompt = require('cli-prompt');
+var Request = require('request').defaults({ jar: true });
+
+
+/****************************************************************/
+/* Config */
+/****************************************************************/
+
+var config = {
+  BASE_API_URL: "https://api.jcpenney.com/v2"
+};
+
+
+/****************************************************************/
+/* Sign In */
+/****************************************************************/
+
+signIn = function(email, password, callback) {
+
+  reqOpts = {
+    uri: config.BASE_API_URL + "/session"
+    , json: { email: email, password: password }
+  };
+
+  Request.post(reqOpts, function(err, httpResponse, body) {
+    if (err) return callback(err);
+    var cookie = httpResponse.headers['set-cookie'][0];
+    Request.cookie(cookie); // Assigns global cookie, applied to all subsequent requests
+    callback(null, cookie);
+  });
+
+};
+
+
+/****************************************************************/
+/* Get Addresses */
+/****************************************************************/
+
+getAddresses = function(callback) {
+
+  reqOpts = {
+    uri: config.BASE_API_URL + "/customer/addresses"
+  };
+
+  Request.get(reqOpts, function(err, httpResponse, body) {
+    if (err) return callback(err);
+    callback(null, JSON.parse(body));
+  });
+
+};
+
+
+/****************************************************************/
+/* Delete Address */
+/****************************************************************/
+
+deleteAddress = function(addressId, callback) {
+
+  reqOpts = {
+    uri: config.BASE_API_URL + "/customer/address/" + addressId
+  };
+
+  Request.del(reqOpts, function(err, httpResponse, body) {
+    if (err) return callback(err);
+    callback();
+  });
+
+};
+
+
+/****************************************************************/
+/* App Flow */
+/****************************************************************/
+
+Prompt("\nEnter your JCP email: ".cyan, function (email) {
+  Prompt.password("Enter your JCP password: ".cyan, function (password) {
+    console.log("\nSigning in...".yellow);
+    signIn(email, password, function(err, cookie) {
+      if (err) return console.error("Error signing in:".red, JSON.stringify(err).red);
+      console.log("\nSigned in successfully.".green);
+      console.log("\nFetching existing addresses...".yellow);
+      getAddresses(function(err, addresses) {
+        if (err) return console.error("Error fetching addresses:".red, JSON.stringify(err).red);
+        console.log("\nAddresses retrieved successfully:\n".green, addresses);
+        if (addresses.length > 0) {
+          var addressId = addresses[0].id;
+          console.log("\nDeleting address with id: ".green + addressId.green);
+          deleteAddress(addressId, function(err) {
+            if (err) return console.error("Error deleting address:".red, JSON.stringify(err).red);
+            console.log("\nAddress deleted successfully".green);
+          });
+        } else {
+          console.log("\nNo addresses to delete".green);
+        }
+        console.log("\nThanks for playing!\n".rainbow)
+      });
+    });
+  });
+});
